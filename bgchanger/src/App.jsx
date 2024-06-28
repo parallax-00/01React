04feee2715df9f63c52bc25aa5d8b997e5622eca
parallax70 <155736682+parallax70@users.noreply.github.com>
2024-06-28 11:01:18 +0530

    final

--- bgchanger/src/App.jsx
+++ bgchanger/src/App.jsx
@@ -5,24 +5,23 @@ import './App.css'
 
 function App() {
   const [color, setColor] = useState("white");
-  const [head,setHead] = useState("black")
 
   return (
     <><div style={{ backgroundColor: color }} id="container">
-        <h1 style={{Color: head}} id='heading'>Background color changer</h1>
+        <h1 id='heading'>Background color changer</h1>
         <div id='button'>
         <button onClick={() => setColor("blue")}
           style={{backgroundColor:"blue"}}
           id='butt1' class='buttons'>Blue</button>
-        <button onClick={() => setColor("green")}
-          style={{backgroundColor:"green"}}
-          id='butt2' class='buttons'>Green</button>
+        <button onClick={() => setColor("Red")}
+          style={{backgroundColor:"red"}}
+          id='butt2' class='buttons'>Red</button>
           <button onClick={() => setColor("yellow")}
           style={{ backgroundColor: "yellow" }}
           id='butt3' class='buttons'>Yellow</button>
-          <button onClick={() => setColor("black")}
-          style={{ backgroundColor: "black" }}
-          id='butt4' class='buttons'>Black</button>
+          <button onClick={() => setColor("purple")}
+          style={{ backgroundColor: "purple" }}
+          id='butt4' class='buttons'>Purple</button>
           <button onClick={() => setColor("white")}
           style={{ backgroundColor: "white" }}
           id='butt5' class='buttons'>Reset</button>
