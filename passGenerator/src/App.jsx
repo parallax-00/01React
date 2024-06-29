af3921ce7447fc82eaaba195d936d624496b3b2a
parallax70 <155736682+parallax70@users.noreply.github.com>
2024-06-29 14:10:45 +0530

    passwordGenerator

---
+++ passGenerator/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 id='Head'>Password Generator</h1>
+      <div id='base'>
+        
+      </div>
+    </>
+  )
+}
+
+export default App
