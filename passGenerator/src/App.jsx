981f9e4de239f04fc30cea8b87081adcabf246c9
parallax70 <155736682+parallax70@users.noreply.github.com>
2024-06-29 14:54:43 +0530

    again

--- passGenerator/src/App.jsx
+++ passGenerator/src/App.jsx
@@ -1,14 +1,32 @@
-import { useState } from 'react'
+import { useState, useCallback } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [length, setlength] = useState(7)
+  const [numAllowed, setNumAllowed] = useState(false)
+  const [charAllowed, setCharAllowed] = useState(false)
+  const [word, setWord] = useState("")
+
+  const passwordGenerator = useCallback(() => {
+    let pass
+    let str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+    if (numAllowed) str += '0123456789'
+    if (charAllowed) str += '!@#$%^&*()_'
+      for (let i = 1; i < array.length; i++) {
+        let char = Math.floor(Math.random() * str.length + 1)
+        pass=str.charAt(char)
+      }
+    setWord(pass)
+  }, [length, numAllowed, charAllowed, setWord])
+    
+  
+
 
   return (
     <>
       <h1 id='Head'>Password Generator</h1>
       <div id='base'>
-        
+
       </div>
     </>
   )
