427393ac382e34d9e820ba2c2745ab62edff8684
parallax70 <155736682+parallax70@users.noreply.github.com>
2024-07-01 18:21:00 +0530

    passwordGenerator

--- passGenerator/src/App.jsx
+++ passGenerator/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useCallback } from 'react'
+import { useState, useCallback, useRef, useEffect, useReducer } from 'react'
 import './App.css'
 
 function App() {
@@ -8,26 +8,59 @@ function App() {
   const [word, setWord] = useState("")
 
   const passwordGenerator = useCallback(() => {
-    let pass
+    let pass =''
     let str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
     if (numAllowed) str += '0123456789'
     if (charAllowed) str += '!@#$%^&*()_'
-      for (let i = 1; i < array.length; i++) {
+      for (let i = 1; i <= length; i++) {
         let char = Math.floor(Math.random() * str.length + 1)
-        pass=str.charAt(char)
+        pass +=str.charAt(char)
       }
     setWord(pass)
   }, [length, numAllowed, charAllowed, setWord])
     
+  useEffect(()=> {
+    passwordGenerator()
+  }, [numAllowed, charAllowed, length, passwordGenerator])
   
+  const passwordRef = useRef(null)
+  
+  const copyToClipBoard = useCallback(() => {
+    passwordRef.current?.select()
+    window.navigator.clipboard.writeText(word)
+  }, [word])
 
 
   return (
     <>
       <h1 id='Head'>Password Generator</h1>
+      <div id='base0'>
       <div id='base'>
-
-      </div>
+        <div id='Copied'>
+            <input
+            id='toBeCopied'
+            type="text"
+            readOnly
+            value={word}
+              placeholder='Password'
+              ref={passwordRef}
+          />
+            <button
+              onClick={copyToClipBoard}
+              class="button-48" role="button"><span class="text">Copy</span>
+            </button>
+          </div>
+          <div id="inputFields">
+            <input type="range" id="length"
+              min={6}
+              max={100}
+              onChange={(e)=>{setlength(e.target.value)}}
+            /> <label>Length:{length}</label>
+            <input type="checkbox" id="number" defaultChecked={numAllowed} onChange={()=>setNumAllowed((prev)=>!prev)} /> <label>Numbers</label>
+            <input type="checkbox" id="character" onChange={()=>setCharAllowed((prev)=>!prev)}/> <label>Character</label>
+            </div>
+        </div>
+        </div>
     </>
   )
 }
